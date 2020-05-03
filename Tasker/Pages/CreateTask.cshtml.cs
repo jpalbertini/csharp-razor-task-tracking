1448bd60339ecef0ada790cbee0d492f5b5ef7b2
winux <jpalbertini@live.fr>
2020-05-03 16:19:42 +0200

    module 2

--- Tasker/Pages/CreateTask.cshtml.cs
+++ Tasker/Pages/CreateTask.cshtml.cs
@@ -9,9 +9,16 @@ namespace Tasker.Pages
 {
     public class CreateTaskModel : PageModel
     {
+        [BindProperty]
+        public Task NewTask { get; set; }
+
         public void OnGet()
         {
 
+        }
+        public IActionResult OnPost()
+        {
+            return RedirectToPage("Index");
         }
     }
 }
